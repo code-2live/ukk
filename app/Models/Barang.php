5e7b613aa9c9247e5731cc95fd20ba06eb4760c8
root <root@debian12.top>
2024-06-14 14:26:24 +0700

    models

---
+++ app/Models/Barang.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Kategori;
+
+class Barang extends Model
+{
+    use HasFactory;
+
+	protected $table = 'barang';
+
+   	 protected $fillable = ['merk','seri','spesifikasi','stok','kategori_id','foto'];
+
+  	  public function kategori()
+ 	{
+  	   return $this->belongsTo(Kategori::class, 'kategori_id');
+ 	}
+	 public function barangMasuk()
+    {
+        return $this->hasMany(BarangMasuk::class);
+    }
+}
